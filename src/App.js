636e16f49f794f6f055cbe20b74e4d9382ac8006
apple <apple@Rakshits-MacBook-Air.local>
2019-12-14 13:42:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Metronome from './components/Metronome'
+
+function App() {
+  return (
+    <div className="App">
+      <Metronome />
+    </div>
+  );
+}
+
+export default App;
